451a29732a33edc90ac13cbaa9278c006a4fc34f
ZaporozhecI <iliya_zap@mail.ru>
2024-04-10 23:48:18 +0500

    Start project

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>605-01</title>
+    </head>
+    <body>
+      <h2>{{ $title }}</title></h2>
+        
+    </body>
+</html>
+
+
+
+
+
+
+
+
